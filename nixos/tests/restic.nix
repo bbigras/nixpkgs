a772ab6debcf1aa7be4ebe18307c1dfd8f27b382
Bruno Bigras <bigras.bruno@gmail.com>
2020-01-30 16:31:52 -0500

    nixos/tests/restic.nix: add test

---
+++ nixos/tests/restic.nix
@@ -0,0 +1,65 @@
+import ./make-test-python.nix (
+  { pkgs, ... }:
+
+    let
+      password = "some_password";
+      repository = "/tmp/restic-backup";
+      passwordFile = pkgs.writeText "password" "correcthorsebatterystaple";
+    in
+      {
+        name = "restic";
+
+        meta = with pkgs.stdenv.lib.maintainers; {
+          maintainers = [ bbigras ];
+        };
+
+        nodes = {
+          server =
+            { ... }:
+              {
+                services.restic.backups = {
+                  remotebackup = {
+                    inherit repository;
+                    passwordFile = "${passwordFile}";
+                    initialize = true;
+                    paths = [ "/opt" ];
+                    pruneOpts = [
+                      "--keep-daily 2"
+                      "--keep-weekly 1"
+                      "--keep-monthly 1"
+                      "--keep-yearly 99"
+                    ];
+                  };
+                };
+              };
+        };
+
+        testScript = ''
+          server.start()
+          server.wait_for_unit("dbus.socket")
+          server.fail(
+              "${pkgs.restic}/bin/restic -r ${repository} -p ${passwordFile} snapshots"
+          )
+          server.succeed("mkdir -p /opt")
+          server.succeed("touch /opt/some_file")
+          server.succeed("timedatectl set-time '2016-12-13 13:45'")
+          server.succeed("systemctl start restic-backups-remotebackup.service")
+          server.succeed(
+              '${pkgs.restic}/bin/restic -r ${repository} -p ${passwordFile} snapshots -c | grep -e "^1 snapshot"'
+          )
+          server.succeed("timedatectl set-time '2017-12-13 13:45'")
+          server.succeed("systemctl start restic-backups-remotebackup.service")
+          server.succeed("timedatectl set-time '2018-12-13 13:45'")
+          server.succeed("systemctl start restic-backups-remotebackup.service")
+          server.succeed("timedatectl set-time '2018-12-14 13:45'")
+          server.succeed("systemctl start restic-backups-remotebackup.service")
+          server.succeed("timedatectl set-time '2018-12-15 13:45'")
+          server.succeed("systemctl start restic-backups-remotebackup.service")
+          server.succeed("timedatectl set-time '2018-12-16 13:45'")
+          server.succeed("systemctl start restic-backups-remotebackup.service")
+          server.succeed(
+              '${pkgs.restic}/bin/restic -r ${repository} -p ${passwordFile} snapshots -c | grep -e "^4 snapshot"'
+          )
+        '';
+      }
+)
