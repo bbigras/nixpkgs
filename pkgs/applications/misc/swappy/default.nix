b9e7af2ca98ae06339f8bf5ce2b876a3f3608dcf
Matthias Beyer <mail@beyermatthias.de>
2020-02-26 13:28:13 +0100

    WIP: swappy: Init at 2020-02-26
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ pkgs/applications/misc/swappy/default.nix
@@ -0,0 +1,35 @@
+{ stdenv, fetchFromGitHub
+, meson
+, ninja
+, wayland
+, cairo
+, pango
+, gtk
+, pkgconfig
+, cmake
+, scdoc
+, libnotify
+, gio-sharp
+}:
+
+stdenv.mkDerivation rec {
+  name = "swappy-${version}";
+  version = "2020-02-26";
+
+  src = fetchFromGitHub {
+    owner = "jtheoof";
+    repo = "swappy";
+    rev = "b5cc433d75d77759cef139e0e232bde79196f886";
+    sha256 = "08r9hmhzzb5ac4g6zwm7w05n99v0hl2h0w4d8i694hg4pyjxb95y";
+  };
+
+  buildInputs = [ meson ninja pkgconfig cmake scdoc gio-sharp libnotify gtk pango cairo wayland];
+
+  meta = {
+    homepage = https://github.com/jtheoof/swappy;
+    description = "A Wayland native snapshot editing tool, inspired by Snappy on macOS ";
+    license = stdenv.lib.licenses.mit;
+    maintainers = [ stdenv.lib.maintainers.matthiasbeyer ];
+    platforms = with stdenv.lib.platforms; linux;
+  };
+}
